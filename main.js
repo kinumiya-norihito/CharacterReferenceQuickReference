820d45e82c5d26431aeac2f51a634f8785d09f7a
kinumiya-norihito <99883931+kinumiya-norihito@users.noreply.github.com>
2022-10-26 16:42:56 +0900

    beautifully

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-window.onload=()=>{
+window.onload = () => {
     const tableBodyElement = document.getElementById("tableBody");
-    for(const item of data){
-        tableBodyElement.innerHTML+=`<tr><th>${item[0]}</th><td>${item[0].replace(/^&/,"&amp;")}</td><td>${item[1]}</td></tr>`;
+    for (const item of data) {
+        tableBodyElement.innerHTML += `<tr><th>${item[0]}</th><td>${item[0].replace(/^&/, "&amp;")}</td><td>${item[1]}</td></tr>`;
     }
 };
